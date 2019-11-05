7b8f9497a62ca79e6a261f52c5f06b8ae3efa084
Alexander Bispo <alexbispo173@gmail.com>
2019-11-05 00:27:28 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,8 @@
 class Person
-  attr_accessible :name, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width
+  attr_accessible :name
 
 
-def initialize(attributes)
+  def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 
